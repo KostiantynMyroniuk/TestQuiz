41e2108f19ade666b1f72960260a2246223bb3b3
KostyaMuroniuk <muronandrey2@gmail.com>
2025-11-26 21:56:42 +0200

    Migrations commit

---
+++ src/TestQuiz.Infrastructure/Migrations/20251126195405_QuestionCountDeleted.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestQuiz.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class QuestionCountDeleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuestionsCount",
+                table: "Tests");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "QuestionsCount",
+                table: "Tests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
